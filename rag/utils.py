df12f033311f6ce4adc86a13a3fe812c34d67bbb
Emie <emie@emie-PC.wind3.hub>
2026-05-10 11:04:38 +0200

    added memory management

--- rag/utils.py
+++ rag/utils.py
@@ -1,17 +1,25 @@
 import requests
 import torch
-from transformers import AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer, BitsAndBytesConfig
 
 def load_model(model, model_type):
     tokenizer = AutoTokenizer.from_pretrained(model)
     
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16, # Accelerate computation
+        bnb_4bit_quant_type="nf4",            # best compression quality
+        bnb_4bit_use_double_quant=True        # VRAM economy
+    )
+
     if model_type == "causal":
-        model = AutoModelForCausalLM.from_pretrained(model)
+        model = AutoModelForCausalLM.from_pretrained(model, quantization_config=bnb_config, device_map="auto")
     elif model_type == "seq2seq":
-        model = AutoModelForSeq2SeqLM.from_pretrained(model)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model, quantization_config=bnb_config, device_map="auto")
     
-    device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-    model.to(device)
+    #device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+    #model.to(device)
+    device = next(model.parameters()).device
 
     return model, tokenizer, device
 
